d126b86f9c015b4812b0ee3becc9b784c3681495
stephane.barthelemy <stephane.barthelemy@intech.lu>
2019-06-21 16:02:02 +0200

    First commit

---
+++ app/src/main/java/com/example/testnotifications/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.testnotifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        logTokenButton.setOnClickListener {
+            // Get token
+            // [START retrieve_current_token]
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    val msg = getString(R.string.msg_token_fmt, token)
+                    Log.d(TAG, msg)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                })
+            // [END retrieve_current_token]
+        }
+
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
